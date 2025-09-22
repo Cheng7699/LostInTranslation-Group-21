1a9fafe063cc2e424b78e1eec04ed226f1e7b9ce
Cheng7699 <diegomgao@gmail.com>
2025-09-22 18:50:29 -0400

    Deleted TODO comments

--- src/main/java/translation/LanguageCodeConverter.java
+++ src/main/java/translation/LanguageCodeConverter.java
@@ -42,7 +42,6 @@ public class LanguageCodeConverter {
             iterator.next(); // skip the first line
             while (iterator.hasNext()) {
                 String line = iterator.next();
-                //  Task A: use line to populate the instance variables
                 languageCodeToLanguage.put(line.split("\t")[1], line.split("\t")[0]);
                 languageToLanguageCode.put(line.split("\t")[0], line.split("\t")[1]);
             }
@@ -58,7 +57,6 @@ public class LanguageCodeConverter {
      * @return the name of the language corresponding to the code
      */
     public String fromLanguageCode(String code) {
-        // TODO Task A: update this code to use the correct instance variable to return the appropriate value
         return languageCodeToLanguage.get(code);
     }
 
@@ -68,7 +66,6 @@ public class LanguageCodeConverter {
      * @return the 2-letter code of the language
      */
     public String fromLanguage(String language) {
-        // TODO Task A: update this code to use the correct instance variable to return the appropriate value
         return languageToLanguageCode.get(language);
     }
 
